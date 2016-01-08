846f48c5a572289f95b8a238a360e20a82a373c0
Craig Lonsdale <craig@meerkats.com.au>
2016-01-08 16:20:35 +0800

    Added in logic to create the katification matrix

--- ConsoleWeather.js
+++ ConsoleWeather.js
@@ -2,7 +2,10 @@ const argv = require('minimist')(process.argv.slice(2));
 const weather = require('./index');
 
 if (argv.katify && argv.katify === true) {
-  console.log('Not Implemented yet');
+  weather(true)
+    .then((data) => {
+      console.log(data);
+    });
 }
 else {
   weather(false)
--- WeatherConfig.js
+++ WeatherConfig.js
@@ -1,9 +1,13 @@
+const getValues = require('lodash.values');
+
 const temperature = {
+  freezing: 10,
   cold: 20,
   perfect: 25,
   hot: 30,
   tooHot: 35,
-  melting: 40
+  melting: 40,
+  dying: 50
 };
 const humidity = {
   dry: 30,
@@ -11,8 +15,18 @@ const humidity = {
   moist: 70,
   swimming: 90
 };
+const humidityValues = getValues(humidity);
+const temperatureValues = getValues(temperature);
+const humidityMin = Math.min.apply(null, humidityValues);
+const humidityMax = Math.max.apply(null, humidityValues);
+const temperatureMin = Math.min.apply(null, temperatureValues);
+const temperatureMax = Math.max.apply(null, temperatureValues);
 
 module.exports = {
   temperature,
-  humidity
+  temperatureMin,
+  temperatureMax,
+  humidity,
+  humidityMin,
+  humidityMax
 };
