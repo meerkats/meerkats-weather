ea720d7bc23994f2242e61cad9287a0dbcde94c6
Craig Lonsdale <craig@meerkats.com.au>
2016-01-08 16:28:15 +0800

    Simplified the console program

--- ConsoleWeather.js
+++ ConsoleWeather.js
@@ -1,16 +1,8 @@
 const argv = require('minimist')(process.argv.slice(2));
 const weather = require('./index');
 
-if (argv.katify && argv.katify === true) {
-  weather(true)
-    .then((data) => {
-      console.log(data);
-    });
-}
-else {
-  weather(false)
-    .then((data) => {
-      console.log(data);
-    });
-}
+weather(argv.katify)
+  .then((data) => {
+    console.log(data);
+  });
 
