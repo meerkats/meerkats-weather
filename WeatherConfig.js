ca46b5e1df21aee39feb4333e527b8c862464546
Craig Lonsdale <craig@meerkats.com.au>
2016-01-08 14:10:47 +0800

    Changed structure

--- WeatherConfig.js
+++ WeatherConfig.js
@@ -1,22 +1,18 @@
-// The temperature when a kat considers it cold
-const coldCelcius = 20;
-
-// The temp when a kat will walk down to doubledouble without a second though.
-const noiceCelcius = 25;
-
-// The temperature when a kat considers it hot
-const hotCelcius = 30;
-
-// The temperature when a kat should just stick to the warrn
-const reallyHotCelcius = 40;
-
-// The thumidity when a kat thinks it feels 'yucky' outside
-const tooHumidPercentage = 95;
+const temperature = {
+  cold: 20,
+  perfect: 25,
+  hot: 30,
+  tooHot: 35,
+  melting: 40
+};
+const humidity = {
+  dry: 30,
+  perfect: 50,
+  moist: 70,
+  swimming: 90
+};
 
 module.exports = {
-  coldCelcius,
-  noiceCelcius,
-  hotCelcius,
-  reallyHotCelcius,
-  tooHumidPercentage
+  temperature,
+  humidity
 };
