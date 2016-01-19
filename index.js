c3df876cf7e2cd0b3b0e9700a56ebdadb023c657
Craig Lonsdale <craig@meerkats.com.au>
2016-01-19 14:42:28 +0800

    Removed default config file

--- index.js
+++ index.js
@@ -2,20 +2,17 @@
 
 const katifyWeather = require('./lib/KatifyWeather');
 const openWeather = require('./lib/OpenWeatherMap');
-const weatherDefaults = require('./OpenWeatherAPIConfig');
 /**
  * Returns a bunch of weather info and some kat language.
  * @param {string} OpenWeatherMaps App Key
  * @param {string} Id of the city to query
- * @param {bool} Shoudl we Katify the weather?
+ * @param {bool} Should we Katify the weather?
  * @return {Object} JSON response with weather details
  */
 module.exports = (appId, cityId, katify) => {
   if (katify) {
-    return openWeather.getWeather(appId || weatherDefaults.appId,
-                                  cityId || weatherDefaults.cityId)
+    return openWeather.getWeather(appId, cityId)
       .then(katifyWeather);
   }
-  return openWeather.getWeather(appId || weatherDefaults.appId,
-                                cityId || weatherDefaults.cityId);
+  return openWeather.getWeather(appId, cityId);
 };
